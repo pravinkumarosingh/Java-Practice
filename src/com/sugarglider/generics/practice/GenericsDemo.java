f996540c5e376c6f318eba7eedc0f87ed7f68d46
pravinkumarosingh <pravinkumarosingh@gmail.com>
2025-08-19 16:14:19 +0530

    bound type parameter examples added

--- src/com/sugarglider/generics/practice/GenericsDemo.java
+++ src/com/sugarglider/generics/practice/GenericsDemo.java
@@ -1,5 +1,6 @@
 package com.sugarglider.generics.practice;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,6 +17,9 @@ public class GenericsDemo {
         Container<List<Integer>> listContainer = new ContainerImpl<>();
         listContainer.set(Arrays.asList(1,2,3));
         System.out.println(listContainer.get());
+
+        GenericsDemo1<List> list1 = new GenericsDemo1<>();
+        GenericsDemo2<ArrayList> list2 = new GenericsDemo2<>();
     }
 }
 
